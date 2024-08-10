291b3877a4e4d61e17e3ead5c01abed72298c778
Adam Dougherty <adamiandougherty@gmail.com>
2024-08-10 15:35:20 +0100

    update titles

--- preview.php
+++ preview.php
@@ -1,5 +1,24 @@
 <?php
 
+/**
+ * Takes in a file name e.g. "about-us.html"
+ * Removes the file extension
+ * Replaces dashes with spaces
+ * Puts the first character of each word in the name to uppsercase, e.g. About Us
+ */
+function clean($filename) {
+    // Remove the file extension
+    $nameWithoutExtension = pathinfo($filename, PATHINFO_FILENAME);
+    
+    // Replace dashes with spaces
+    $nameWithSpaces = str_replace('-', ' ', $nameWithoutExtension);
+    
+    // Capitalize the first letter of each word
+    $newname = ucwords($nameWithSpaces);
+    
+    return $newname;
+}
+
 $html = file_get_contents("template.html");
 
 $title = "Page 1";
@@ -7,9 +26,11 @@ $anylink = "";
 $content = "Hello_world";
 
 if(isset($_GET['page'])) {
+    $title = 
     $content = file_get_contents("pages/".$_GET['page']);
 }
 
 $html = str_replace("{content}", $content, $html);
+$html = str_replace("{title}", clean($_GET['page']), $html);
 
 echo $html;
